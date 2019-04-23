f0cb3c6438cee1bf3029c790aa73892c7d48ea55
Charalamboss <ch.pouroutides@gmail.com>
2019-04-23 20:56:30 +0300

    added multiple images to articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,8 @@
 class Article < ApplicationRecord
 	has_many :comments , dependent: :destroy
+	mount_uploaders :image, ImageUploader
+	serialize :image, JSON
 	validates :title, presence: true,
                     length: { minimum: 5 }
+
 end
